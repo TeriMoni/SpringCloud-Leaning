c7457544cf6f0d21feeae945d4cb7dc7e3bbbf2b
TeriMoni <351111617@qq.com>
2018-01-15 13:24:33 +0800

    api网关路由

---
+++ api-gateway/src/main/java/com/liu/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
