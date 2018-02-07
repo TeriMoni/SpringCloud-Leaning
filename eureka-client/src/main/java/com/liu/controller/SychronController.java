fb5679ba85299f58ec759c513ce077fcb8db9150
TeriMoni <351111617@qq.com>
2018-02-07 19:59:02 +0800

    添加新的接口

--- eureka-client/src/main/java/com/liu/controller/SychronController.java
+++ eureka-client/src/main/java/com/liu/controller/SychronController.java
@@ -1,6 +1,7 @@
 package com.liu.controller;
 
 import com.liu.service.SychronDataService;
+import com.liu.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +19,11 @@ public class SychronController {
     @RequestMapping(value = "/sychron/{industry}/{startTime}/{endTime}", method = RequestMethod.GET)
     public void sychron(@PathVariable String industry, @PathVariable String startTime, @PathVariable String endTime) throws ParseException {
         sychronDataService.sychron(startTime,endTime,industry);
+    }
+
+    @RequestMapping(value = "/permission", method = RequestMethod.GET)
+    public Object sychronPermission() throws ParseException {
+        sychronDataService.permission();
+        return  ResultUtil.success(null);
     }
 }
