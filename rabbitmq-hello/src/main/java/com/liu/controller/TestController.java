d3dc8bac773554073a3a0a18c07cd6ccf896ef7f
TeriMoni <351111617@qq.com>
2018-03-01 15:10:12 +0800

    测试控制器

---
+++ rabbitmq-hello/src/main/java/com/liu/controller/TestController.java
@@ -0,0 +1,47 @@
+package com.liu.controller;
+
+import com.liu.consumer.ReceiverA;
+import com.liu.productor.SenderA;
+import com.liu.productor.SenderB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private SenderA sendA;
+
+    @Autowired
+    private SenderB sendB;
+
+
+    @Autowired
+    private ReceiverA receiverA;
+
+    /**
+     * 发送测试消息队列 生产者多消费者
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        sendA.send("hello");
+        return "success";
+    }
+
+
+    @GetMapping ("/oneToMany")
+    public void oneToMany() {
+        for(int i=0;i<10;i++){
+            sendA.send("hello:"+i);
+        }
+    }
+
+    @GetMapping ("/manyToMany")
+    public void manyToMany() {
+        for(int i=0;i<10;i++){
+            sendA.send("hello:"+i);
+            sendB.send("hello:"+i);
+        }
+    }
+
+}
